ee209b60441015c94ad58924ec97f8a85d491dbc
Abdul Basit <abdulbasitepic@gmail.com>
2026-09-15 22:32:24 +0500

    Complete LibraryDesk application

--- LibraryDesk/src/main/java/com/LibraryDesk/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.LibraryDesk.repository;
-import com.LibraryDesk.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-
-public interface StudentRepository extends JpaRepository<Student , Integer> {
-
-}
