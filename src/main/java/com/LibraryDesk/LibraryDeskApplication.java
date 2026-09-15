245d5190551190d2adf377c90ab60432cec41ed0
Abdul Basit <abdulbasitepic@gmail.com>
2026-09-15 22:33:10 +0500

    Configure Java 26 for deployment

---
+++ src/main/java/com/LibraryDesk/LibraryDeskApplication.java
@@ -0,0 +1,14 @@
+package com.LibraryDesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryDeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryDeskApplication.class, args);
+	}
+
+}
+cd "C:\Users\Basit\.vscode\java"
---
+++ src/test/java/com/LibraryDesk/LibraryDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LibraryDesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryDeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
